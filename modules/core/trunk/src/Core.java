8d19d5d3f56fcd35d9521d0ee0398fde28b67931
simon-felix <simon-felix@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2009-07-28 14:18:01 +0000

    - removed spring, included maven reference instead - added taverna2 engine - example workflow is running

--- modules/core/trunk/src/Core.java
+++ modules/core/trunk/src/Core.java
@@ -1,13 +1,92 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import net.sf.taverna.platform.spring.RavenAwareClassPathXmlApplicationContext;
+import net.sf.taverna.t2.facade.ResultListener;
+import net.sf.taverna.t2.facade.WorkflowInstanceFacade;
+import net.sf.taverna.t2.facade.impl.WorkflowInstanceFacadeImpl;
+import net.sf.taverna.t2.invocation.InvocationContext;
+import net.sf.taverna.t2.invocation.WorkflowDataToken;
+import net.sf.taverna.t2.monitor.MonitorNode;
+import net.sf.taverna.t2.provenance.reporter.ProvenanceReporter;
+import net.sf.taverna.t2.reference.ReferenceService;
+import net.sf.taverna.t2.utility.TypedTreeModelEvent;
+import net.sf.taverna.t2.utility.TypedTreeModelListener;
+import net.sf.taverna.t2.workflowmodel.Dataflow;
+import net.sf.taverna.t2.workflowmodel.InputPort;
+import net.sf.taverna.t2.workflowmodel.NamedWorkflowEntity;
+import net.sf.taverna.t2.workflowmodel.OutputPort;
+import net.sf.taverna.t2.workflowmodel.serialization.xml.XMLDeserializer;
+import net.sf.taverna.t2.workflowmodel.serialization.xml.XMLDeserializerImpl;
+
+import org.embl.ebi.escience.utils.TavernaSPIRegistry;
+import org.jdom.input.SAXBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.jdom.Element;
 
 
 public class Core
 {
-  public static void main(String[] _args)
+  public static void main(String[] _args) throws Exception
   {
-    ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"spring-config.xml"});
+    ApplicationContext context = new RavenAwareClassPathXmlApplicationContext("spring-config.xml");
     ((AbstractApplicationContext)context).registerShutdownHook();
+    
+    final ReferenceService referenceService = (ReferenceService) context.getBean("t2reference.service.referenceService");
+
+    
+    //TavernaBaseProfile profile=new TavernaBaseProfile(context);
+    
+    XMLDeserializer deserializer = new XMLDeserializerImpl();
+    InputStream inStream = new FileInputStream("workflows/example/tv2-test.t2flow");
+    SAXBuilder builder = new SAXBuilder();
+    Element el= builder.build(inStream).detachRootElement();
+    final Dataflow df=deserializer.deserializeDataflow(el);
+    
+    for(InputPort ip:df.getInputPorts())
+      System.out.println("In: "+ip.getName());
+    
+    
+    for(OutputPort op:df.getOutputPorts())
+      System.out.println("Out: "+op.getName());
+    
+    final InvocationContext ic=new InvocationContext()
+    {
+      public <T> List<? extends T> getEntities(Class<T> entityType)
+      {
+        return null;
+      }
+      
+      public ReferenceService getReferenceService()
+      {
+        return referenceService;
+      }
+      
+      public ProvenanceReporter getProvenanceReporter()
+      {
+        return null;
+      }
+    };
+    
+    WorkflowInstanceFacade wif=new WorkflowInstanceFacadeImpl(df,ic,"a");
+    
+    wif.fire();
+    
+    wif.addResultListener(new ResultListener()
+    {
+      public void resultTokenProduced(WorkflowDataToken token,String portName)
+      {
+        Object s=referenceService.renderIdentifier(token.getData(),Object.class,ic);
+        System.out.println(portName+"="+s);
+      }
+    });
+    
+    Thread.sleep(1000);
+    
+    System.out.println("ende");
   }
 }
