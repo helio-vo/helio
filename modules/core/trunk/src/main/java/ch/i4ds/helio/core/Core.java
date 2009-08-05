e59db74b9ca4008ec5587c3a379173adb0f710cc
simon-felix <simon-felix@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2009-08-05 12:33:59 +0000

    + webservice with taverna workflow can be called from another taverna workflow (inside workbench)

--- modules/core/trunk/src/main/java/ch/i4ds/helio/core/Core.java
+++ modules/core/trunk/src/main/java/ch/i4ds/helio/core/Core.java
@@ -1,6 +1,9 @@
 package ch.i4ds.helio.core;
-/*import java.io.*;
+import java.io.*;
 import java.util.List;
+import java.util.concurrent.*;
+
+import javax.jws.WebService;
 
 import net.sf.taverna.platform.spring.RavenAwareClassPathXmlApplicationContext;
 import net.sf.taverna.t2.facade.*;
@@ -14,33 +17,25 @@ import net.sf.taverna.t2.workflowmodel.serialization.xml.*;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;*/
+import org.springframework.context.support.AbstractApplicationContext;
 
 
+@WebService(serviceName="GetGurk")
 public class Core
 {
-  /*public static void main(String[] _args) throws Exception
+  
+  public String execWF(String _input) throws Exception
   {
-    ApplicationContext context = new RavenAwareClassPathXmlApplicationContext("spring-config.xml");
+    ApplicationContext context = new RavenAwareClassPathXmlApplicationContext("taverna-config.xml");
     ((AbstractApplicationContext)context).registerShutdownHook();
     
     final ReferenceService referenceService = (ReferenceService) context.getBean("t2reference.service.referenceService");
-
-    
-    //TavernaBaseProfile profile=new TavernaBaseProfile(context);
     
     XMLDeserializer deserializer = new XMLDeserializerImpl();
-    InputStream inStream = new FileInputStream("workflows/example/tv2-test.t2flow");
+    InputStream inStream = new FileInputStream("../applications/core/WEB-INF/classes/workflows/example/tv2-test.t2flow");
     SAXBuilder builder = new SAXBuilder();
     Element el= builder.build(inStream).detachRootElement();
     final Dataflow df=deserializer.deserializeDataflow(el);
-    
-    for(InputPort ip:df.getInputPorts())
-      System.out.println("In: "+ip.getName());
-    
-    
-    for(OutputPort op:df.getOutputPorts())
-      System.out.println("Out: "+op.getName());
     
     final InvocationContext ic=new InvocationContext()
     {
@@ -64,13 +59,24 @@ public class Core
     
     wif.fire();
     
+    final LinkedBlockingQueue<String> result=new LinkedBlockingQueue<String>();
+    
     wif.addResultListener(new ResultListener()
     {
       public void resultTokenProduced(WorkflowDataToken token,String portName)
       {
         Object s=referenceService.renderIdentifier(token.getData(),Object.class,ic);
-        System.out.println(portName+"="+s);
+        try
+        {
+          result.put((String)s);
+        }
+        catch(InterruptedException _ie)
+        {
+          
+        }
       }
     });
-  }*/
+    
+    return result.take();
+  }
 }
