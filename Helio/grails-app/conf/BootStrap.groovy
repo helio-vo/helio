0efb1daeb64ad6f8149e322527f81988ecb7bc0b
ddguevara <ddguevara@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2009-09-01 00:09:25 +0000

    added the javascript for images, fixed several errors

--- Helio/grails-app/conf/BootStrap.groovy
+++ Helio/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,22 @@
 class BootStrap {
 
      def init = { servletContext ->
+
+
+        def tempDate = new Date();
+        tempDate.setYear(100);
+   
+
+    new Flare(positionX:1,positionY:1,positionZ:1,radialOffset:1,goes:"James",startDate:tempDate,image:"mythumb.jpg").save()
+    new Flare(positionX:2,positionY:3,positionZ:1,radialOffset:1,goes:"James1",startDate:tempDate,image:"mythumb.jpg").save()
+    new Flare(positionX:2,positionY:3,positionZ:1,radialOffset:1,goes:"James2",startDate:tempDate,image:"mythumb.jpg").save()
+    new Flare(positionX:2,positionY:3,positionZ:1,radialOffset:1,goes:"James3",startDate:tempDate,image:"mythumb.jpg").save()
+    new Flare(positionX:2,positionY:3,positionZ:1,radialOffset:1,goes:"James4",startDate:new Date(),image:"mythumb.jpg").save()
+        
+       
+           
+        
+
      }
      def destroy = {
      }
