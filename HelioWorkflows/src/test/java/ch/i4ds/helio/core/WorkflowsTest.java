423cbe266f481f8d566807b6e9f99a7507271f2c
simon-felix <simon-felix@5d9e9ddb-692e-4027-9be8-b4f4f3982da7>
2010-01-13 14:53:53 +0000

    removed DPAS

---
+++ HelioWorkflows/src/test/java/ch/i4ds/helio/core/WorkflowsTest.java
@@ -0,0 +1,28 @@
+package ch.i4ds.helio.core;
+
+import org.apache.xbean.spring.context.FileSystemXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import ch.i4ds.helio.workflows.*;
+import junit.framework.TestCase;
+
+public class WorkflowsTest extends TestCase
+{
+  private Workflows wf;
+  
+  public WorkflowsTest()
+  {
+    ApplicationContext ac=new FileSystemXmlApplicationContext("src/webapp/WEB-INF/applicationContext.xml");
+    wf=(Workflows)ac.getBean("Workflows");
+  }
+  
+  public void testInstrumentList() throws Exception
+  {
+    
+    /*assertTrue(ff.getInstruments().length>0);
+    for(String instrumentName:ff.getInstruments())
+    {
+      assertNotNull(instrumentName);
+      assertTrue(!instrumentName.equals(""));
+    }*/
+  }
+}
